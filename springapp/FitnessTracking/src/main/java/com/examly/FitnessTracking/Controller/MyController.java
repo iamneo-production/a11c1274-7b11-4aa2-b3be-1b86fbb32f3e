ff1e68d0810882441f809c7dede7afe8317e8c3d
NEERAJAP2001 <neeruap2001@gmail.com>
2023-05-06 18:23:45 +0000

    Updated Users Dao and its entities

--- springapp/FitnessTracking/src/main/java/com/examly/FitnessTracking/Controller/MyController.java
+++ springapp/FitnessTracking/src/main/java/com/examly/FitnessTracking/Controller/MyController.java
@@ -1,17 +1,59 @@
 package com.examly.FitnessTracking.Controller;
 import org.springframework.web.bind.annotation.RestController;
+import com.examly.FitnessTracking.entities.Users;
+import com.examly.FitnessTracking.services.UsersService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class MyController {
-
-    @GetMapping("/")
-	public String landing() {
-		return "This is Landing  page";
-	}
+	
+	@Autowired
+	private UsersService usersservice;
 
 	@GetMapping("/home")
 	public String home() {
 		return "This is Home page";
 	}
+	
+	// Get all users
+	@GetMapping("/users")
+	public List<Users> users() {
+	return this.usersservice.users();
+	}
+	
+	// Save a user
+	@PostMapping("/saveUser")
+	public Users saveUser(@RequestBody Users user ) {
+		return this.usersservice.addUser(user);
+	}
+	
+	
+	// Get user by ID
+	@GetMapping("/users/{id}")
+	public Users getUser(@PathVariable int id) {
+		return this.usersservice.getUser(id);
+	}
+	
+	// Delete user by ID
+	@DeleteMapping("/deleteUser/{id}")
+	public void delete(@PathVariable int id) {
+		this.usersservice.deleteUser(id);
+	}
+	
+	// Update user Info
+	@PostMapping("/changeUserInfo")
+	public Users changeUserInfo(@RequestBody Users user ) {
+		return this.usersservice.changeUserInfo(user);
+	
+	}
+	
 }
+
--- springapp/FitnessTracking/src/main/java/com/examly/FitnessTracking/entities/Users.java
+++ springapp/FitnessTracking/src/main/java/com/examly/FitnessTracking/entities/Users.java
@@ -1,1 +1,91 @@
-// here comes code
+package com.examly.FitnessTracking.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Users{
+	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int UserId;
+	private String UserName;
+	private String UserEmail;
+	private String UserPassword;
+	private int UserHeight;
+	private int  UserWeight;
+	private int UserAge;
+	private String UserGender;
+	
+	
+	public Users(int userId, String userName, String userEmail, String userPassword, int userHeight, int userWeight,
+			int userAge, String userGender) {
+		super();
+		UserId = userId;
+		UserName = userName;
+		UserEmail = userEmail;
+		UserPassword = userPassword;
+		UserHeight = userHeight;
+		UserWeight = userWeight;
+		UserAge = userAge;
+		UserGender = userGender;
+	}
+	
+	
+	public Users() {
+		super();
+	}
+	
+	
+	
+	public int getUserId() {
+		return UserId;
+	}
+	public void setUserId(int userId) {
+		UserId = userId;
+	}
+	public String getUserName() {
+		return UserName;
+	}
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	public String getUserEmail() {
+		return UserEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		UserEmail = userEmail;
+	}
+	public String getUserPassword() {
+		return UserPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		UserPassword = userPassword;
+	}
+	public int getUserHeight() {
+		return UserHeight;
+	}
+	public void setUserHeight(int userHeight) {
+		UserHeight = userHeight;
+	}
+	public int getUserWeight() {
+		return UserWeight;
+	}
+	public void setUserWeight(int userWeight) {
+		UserWeight = userWeight;
+	}
+	public int getUserAge() {
+		return UserAge;
+	}
+	public void setUserAge(int userAge) {
+		UserAge = userAge;
+	}
+	public String getUserGender() {
+		return UserGender;
+	}
+	public void setUserGender(String userGender) {
+		UserGender = userGender;
+	}
+	
+}
