6dae96d67f0ec619296f4721ec2ac8985c9ef85e
sidd-005 <rescueskforce@gmail.com>
2023-06-19 08:16:26 +0000

    this commit is for my tasks

--- springapp/FitnessTracking/src/main/java/com/example/Fitnesstracking/Exception/ApiException.java
+++ springapp/FitnessTracking/src/main/java/com/example/Fitnesstracking/Exception/ApiException.java
@@ -1,1 +1,14 @@
 package com.example.Fitnesstracking.Exception;
+public class ApiException extends RuntimeException {
+
+	public ApiException(String message) {
+		super(message);
+
+	}
+
+	public ApiException() {
+		super();
+
+	}
+
+}
