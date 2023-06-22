31eb0b202602357b33d667711aaef759812dbddc
Rakesh2577 <rakesh.p.2019.cse@ritchennai.edu.in>
2023-06-22 09:54:44 +0000

    This commit solves my tasks

--- springapp/FitnessTracking/src/main/java/com/example/Fitnesstracking/Dao/UsersDao.java
+++ springapp/FitnessTracking/src/main/java/com/example/Fitnesstracking/Dao/UsersDao.java
@@ -1,2 +1,23 @@
 package com.example.Fitnesstracking.Dao;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.Fitnesstracking.entities.Users;
+
+@Repository
+public interface UsersDao extends JpaRepository<Users,Integer> {
+
+	Optional<Users> findByEmail(String email);
+	
+    @Query(value = "select * from users where email =:userEmail", nativeQuery = true)
+    Users findByUserEmail(@Param("userEmail") String userEmail);
+
+    
+}
+
+
