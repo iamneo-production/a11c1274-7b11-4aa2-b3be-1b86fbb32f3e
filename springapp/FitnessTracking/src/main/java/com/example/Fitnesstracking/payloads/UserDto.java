0399a909a16402ac6665bcc37a3ee0a36b37f107
NandhithaST09 <stnandhitha@gmail.com>
2023-06-22 12:58:35 +0000

    payload code

--- springapp/FitnessTracking/src/main/java/com/example/Fitnesstracking/payloads/UserDto.java
+++ springapp/FitnessTracking/src/main/java/com/example/Fitnesstracking/payloads/UserDto.java
@@ -1,2 +1,156 @@
 package com.example.Fitnesstracking.payloads;
 
+
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import com.example.Fitnesstracking.entities.Goals;
+import com.example.Fitnesstracking.entities.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+public class UserDto {
+
+
+	private int id;
+
+	@NotEmpty
+	@Size(min = 4, message = "Username must be min of 4 characters !!")
+	private String name;
+
+	@Email(message = "Email address is not valid !!")
+	@NotEmpty(message = "Email is required !!")
+	private String email;
+
+	@NotEmpty
+	@Size(min = 3, max = 10, message = "Password must be min of 3 chars and max of 10 chars !!")
+	private String password;
+
+    private int height;
+    private int weight;
+    private int age;
+    
+    
+	@NotEmpty
+    private String gender;
+    
+    
+
+    
+
+
+    
+    private List<Goals> goals = new ArrayList();
+
+	
+	private Set<RoleDto> roles = new HashSet<>();
+	
+	
+	public UserDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@JsonIgnore
+	public String getPassword() {
+		return this.password;
+	}
+	
+	@JsonProperty
+	public void setPassword(String password) {
+		this.password=password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public Set<RoleDto> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<RoleDto> roles) {
+		this.roles = roles;
+	}
+
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public List<Goals> getGoals() {
+		return goals;
+	}
+
+	public void setGoals(List<Goals> goals) {
+		this.goals = goals;
+	}
+
+	
+
+}
+
