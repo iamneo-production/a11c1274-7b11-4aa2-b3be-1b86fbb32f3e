035721eaa1bc3c6906fbfd43c8b13b2482f9be49
5d7kiran <198r1a05d7@gmail.com>
2023-06-27 05:09:25 +0000

    This commit solves My tasks

--- springapp/FitnessTracking/src/main/java/com/example/Fitnesstracking/entities/Users.java
+++ springapp/FitnessTracking/src/main/java/com/example/Fitnesstracking/entities/Users.java
@@ -1,5 +1,213 @@
 package com.example.Fitnesstracking.entities;
  
-public class Users{
+import jakarta.persistence.GeneratedValue;
+import java.util.Collection;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
+@Table
+public class Users implements UserDetails{
+
+    //instance variables
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private int id; //primary key
     
+    @Column(name = "user_name", nullable = false, length = 100)
+    private String name;
+    
+    @Column(unique = true)
+    private String email;
+    
+    
+    private String password;
+    private int height;
+    private int weight;
+    private int age;
+    private String gender;
+    
+    
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role", referencedColumnName = "id"))
+	private Set<Role> roles = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Goals> goals;
+    
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private OneTimePassword oneTimePassword;
+    
+
+    public Users() {
+        super();
+    }
+    
+    
+    public List<Goals> getGoals() {
+        return goals;
+    }
+
+    public void setGoals(List<Goals> goals) {
+        this.goals = goals;
+    }
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Workout> workouts;
+
+    public List<Workout> getWorkouts() {
+        return workouts;
+    }
+
+    public void setWorkouts(List<Workout> workouts) {
+        this.workouts = workouts;
+    }
+
+
+    
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+
+		List<SimpleGrantedAuthority> authories = this.roles.stream()
+				.map((role) -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+		return authories;
+	}
+
+    public Users(int id, String name, String email, String password, int height, int weight, int age,
+                 String gender) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.height = height;
+        this.weight = weight;
+        this.age = age;
+        this.gender = gender;
+    }
+
+
+    //    Setters and getters for instance variables
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
