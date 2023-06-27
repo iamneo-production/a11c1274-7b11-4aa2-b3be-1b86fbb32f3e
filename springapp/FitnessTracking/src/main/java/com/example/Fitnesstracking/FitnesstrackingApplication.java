61d6eaba652ff4d155d9c3981554113fa01dbab2
Pragyabhagat26 <pragyassb26@gmail.com>
2023-06-27 05:46:26 +0000

    Initial commit

--- springapp/FitnessTracking/src/main/java/com/example/Fitnesstracking/FitnesstrackingApplication.java
+++ springapp/FitnessTracking/src/main/java/com/example/Fitnesstracking/FitnesstrackingApplication.java
@@ -1,3 +1,70 @@
 package com.example.Fitnesstracking;
 
 //My code goes here
+import java.util.List;
+
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.Fitnesstracking.Dao.RoleDao;
+import com.example.Fitnesstracking.config.AppConstants;
+import com.example.Fitnesstracking.entities.Role;
+
+@SpringBootApplication
+public class FitnesstrackingApplication implements CommandLineRunner{
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	private RoleDao roleRepo;
+	
+	
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FitnesstrackingApplication.class, args);
+	}
+
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		//System.out.println(this.passwordEncoder.encode("xyz"));
+
+		try {
+
+			Role role = new Role();
+			role.setId(AppConstants.ADMIN_USER);
+			role.setName("ROLE_ADMIN");
+
+			Role role1 = new Role();
+			role1.setId(AppConstants.NORMAL_USER);
+			role1.setName("ROLE_NORMAL");
+
+			List<Role> roles = List.of(role, role1);
+
+			List<Role> result = this.roleRepo.saveAll(roles);
+
+			result.forEach(r -> {
+				System.out.println(r.getName());
+			});
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
