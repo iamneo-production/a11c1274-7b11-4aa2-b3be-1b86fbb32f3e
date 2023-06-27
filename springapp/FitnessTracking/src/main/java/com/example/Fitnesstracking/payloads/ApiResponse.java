e9ef518f6ae4b0474e13323461ee141304e9e8cf
Harini2001219 <harinijan2001@gmail.com>
2023-06-27 05:58:32 +0000

    This commit adds API response template

--- springapp/FitnessTracking/src/main/java/com/example/Fitnesstracking/payloads/ApiResponse.java
+++ springapp/FitnessTracking/src/main/java/com/example/Fitnesstracking/payloads/ApiResponse.java
@@ -1,5 +1,32 @@
 package com.example.Fitnesstracking.payloads;
 
-public class ApiResponse{
-    
+public class ApiResponse {
+
+	private String message;
+	private boolean success;
+	
+	public ApiResponse(String message, boolean success) {
+		super();
+		this.message = message;
+		this.success = success;
+	}
+	public ApiResponse() {
+		super();
+	}
+	
+	
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public boolean isSuccess() {
+		return success;
+	}
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
 }
