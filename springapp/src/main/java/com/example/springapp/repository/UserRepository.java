3fcfcdb3a57d0de080addad4f8e73b9e2aa34c32
NEERAJAP2001 <neeruap2001@gmail.com>
2023-07-28 12:28:33 +0000

    latest commit

--- springapp/src/main/java/com/example/springapp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.springapp.model.User;
-
-public interface UserRepository extends JpaRepository<User,Long>{
-    
-}
--- springapp/src/main/java/com/example/springapp/security/AppSecurityConfig.java
+++
@@ -1,5 +0,0 @@
-package com.example.springapp.security;
-
-public class AppSecurityConfig {
-    
-}
